80880d1715efe4b50b4447ee140456afe24b2ddc
iambijoy656 <bijoychandrodas56@gmail.com>
2023-09-21 17:59:12 +0600

    update

--- src/Shared/Header1/TopNavbar.jsx
+++ src/Shared/Header1/TopNavbar.jsx
@@ -1,23 +1,39 @@
 // import { HiLink } from "react-icons/hi";
 // import { MdArrowDropDown } from "react-icons/md";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaLinkedin,
+  FaTwitter,
+} from "react-icons/fa";
+import { Link } from "react-router-dom";
 const TopNavbar = () => {
   return (
     <div className=" hidden lg:block">
       <div
         style={{ fontWeight: "700" }}
-        className="bg-[#005e9a] flex justify-end  px-5 text-white text-xs"
+        className="bg-[#005e9a] flex justify-between items-center  px-5 text-white text-xs"
       >
-        {/* <div className="p-4 px-5 flex justify-center items-center gap-2 cursor-pointer">
-          <span>
-            <HiLink size={15} />
-          </span>
-          <span>Quick Links</span> <span>|</span>
-          <span>
-            <MdArrowDropDown size={15} />
-          </span>
-        </div> */}
-        <div className="p-5 px-5 bg-[#8d1d59] cursor-pointer">Notice</div>
-        <div className="p-5 px-5 cursor-pointer">CAREER </div>
+        <div className="">
+          <div className=" flex items-center justify-start gap-5 p-2 px-5">
+            <Link to={""} target="_blank" href="https://www.facebook.com">
+              <FaFacebookF color="#e8eded" size="20px" />{" "}
+            </Link>
+            <Link to={""} target="_blank" href="https://www.twitter.com">
+              <FaTwitter color="#e8eded" size="20px" />{" "}
+            </Link>
+            <Link to={""} target="_blank" href="https://www.google.com">
+              <FaLinkedin color="#e8eded" size="20px" />{" "}
+            </Link>
+            <Link to={""} target="_blank" href="https://www.instagram.com">
+              <FaInstagram color="#e8eded" size="20px" />{" "}
+            </Link>
+          </div>
+        </div>
+        <div className="flex justify-end  px-5 text-white text-xs">
+          <div className="p-5 px-5 bg-[#8d1d59] cursor-pointer">Notice</div>
+          <div className="p-5 px-5 cursor-pointer">CAREER </div>
+        </div>
       </div>
     </div>
   );
